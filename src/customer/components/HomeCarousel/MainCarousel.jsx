19d8f8fac20370a80c01d70781ba16850635e64a
akshaykamble04 <akshaykamble1431@gmail.com>
2023-12-18 08:59:56 +0530

    started with basic components of e-commerce

---
+++ src/customer/components/HomeCarousel/MainCarousel.jsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import AliceCarousel from 'react-alice-carousel';
+import 'react-alice-carousel/lib/alice-carousel.css';
+import { mainCarouselData } from './MainCarouselData';
+
+// const responsive = {
+//     0: { items: 1 },
+//     568: { items: 2 },
+//     1024: { items: 3 },
+// };
+
+
+const MainCarousel = () => {
+
+    const items = mainCarouselData.map((item) => <img className="cursor-pointer"
+        src={item.image}
+        alt=''
+        role='representation' />)
+
+    return (
+        <AliceCarousel
+
+            items={items}
+            disableButtonsControls
+            autoPlay
+            autoPlayInterval={1000}
+
+        />
+
+    );
+}
+
+
+export default MainCarousel;
---
+++ src/customer/pages/homepage/Homepage.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import MainCarousel from '../../components/HomeCarousel/MainCarousel'
+
+export const Homepage = () => {
+    return (
+        <div>
+            <MainCarousel />
+        </div>
+    )
+}
+export default Homepage;
